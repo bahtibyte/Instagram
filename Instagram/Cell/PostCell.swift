d16e6e99b0e4e40f855721d2f4a73141de8a0b15
Bahti <bahti@bahtis-macbook-air-4.local>
2019-03-24 21:39:34 -0400

    initial

---
+++ Instagram/Cell/PostCell.swift
@@ -0,0 +1,29 @@
+//
+//  PostCell.swift
+//  Instagram
+//
+//  Created by Bahti on 3/24/19.
+//  Copyright © 2019 Bahti. All rights reserved.
+//
+
+import UIKit
+
+class PostCell: UITableViewCell {
+
+    @IBOutlet weak var photoView: UIImageView!
+    @IBOutlet weak var usernameLabel: UILabel!
+    @IBOutlet weak var captionLabel: UILabel!
+    
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ Instagram/Views/LoginVC.swift
@@ -0,0 +1,67 @@
+//
+//  LoginVC.swift
+//  Instagram
+//
+//  Created by Bahti on 3/24/19.
+//  Copyright © 2019 Bahti. All rights reserved.
+//
+
+import UIKit
+import Parse
+
+class LoginVC: UIViewController {
+
+    @IBOutlet weak var usernameField: UITextField!
+    @IBOutlet weak var passwordField: UITextField!
+    
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func onSignIn(_ sender: Any) {
+        
+        let username = usernameField.text!
+        let password = passwordField.text!
+        
+        PFUser.logInWithUsername(inBackground: username, password: password) {
+            (user, error)  in
+            if user != nil {
+                self.performSegue(withIdentifier: "loginSegue", sender: nil)
+            } else {
+                print("Unable to login \(error?.localizedDescription)")
+            }
+        }
+    }
+    
+    @IBAction func onSignUp(_ sender: Any) {
+        let user = PFUser()
+        user.username = usernameField.text
+        user.password = passwordField.text
+        
+        user.signUpInBackground { (success, error) in
+            if success {
+                self.performSegue(withIdentifier: "loginSegue", sender: nil)
+            }else{
+                print("Unable to signup \(error?.localizedDescription)")
+            }
+        }
+        
+    }
+    
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
